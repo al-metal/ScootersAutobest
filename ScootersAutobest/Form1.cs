aa741ffcdb34daae513bdfcb31aaf15d6b28398f
al-metal <al-metal@users.noreply.github.com>
2017-06-12 22:12:23 +0400

    Получение всех товаров раздела скутеры

--- ScootersAutobest/Form1.cs
+++ ScootersAutobest/Form1.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using Bike18;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,6 +16,10 @@ namespace ScootersAutobest
 {
     public partial class Form1 : Form
     {
+        Thread forms;
+        nethouse nethouse = new nethouse();
+        httpRequest webRequest = new httpRequest();
+
         public Form1()
         {
             InitializeComponent();
@@ -24,6 +32,33 @@ namespace ScootersAutobest
             Properties.Settings.Default.password = tbPassword.Text;
             Properties.Settings.Default.Save();
             #endregion
+
+            #region Обработка сайта
+
+            Thread tabl = new Thread(() => ActualSooters());
+            forms = tabl;
+            forms.IsBackground = true;
+            forms.Start();
+
+            #endregion
+        }
+
+        private void ActualSooters()
+        {
+            CookieContainer cookieNethouse = nethouse.CookieNethouse(tbLogin.Text, tbPassword.Text);
+            if (cookieNethouse.Count == 1)
+            {
+                MessageBox.Show("Логин или пароль для сайта Nethouse введены не верно", "Ошибка логина/пароля");
+                return;
+            }
+
+            string otv = webRequest.getRequest("https://bike18.ru/products/category/skutery-iz-yaponii?page=all");
+            MatchCollection product = new Regex("(?<=<a href=\").*(?=\"><div class=\"-relative item-image\")").Matches(otv);
+            for (int n = 0; product.Count > n; n++)
+            {
+                string urlTovar = product[n].ToString();
+                List<string> listProduct = nethouse.GetProductList(cookieNethouse, urlTovar);
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
